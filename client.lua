9941345e0614a44a5f4eb98080b88f8810b4090d
Luke <62490330+Luke20201@users.noreply.github.com>
2023-01-10 18:50:34 +0000

    Optimise code reduce amount of code

--- client.lua
+++ client.lua
@@ -1,68 +1,43 @@
-timeToDie = 60000 --| 60 seconds until the player will die after taking cyanide           
-
+timeToDie = 60000 --| 60 seconds until the player will die after taking cyanide
 --------------------------------------------
 -- DO NOT TOUCH BELOW--
-Citizen.CreateThread(function()
-RequestAnimDict('mp_suicide')
-while not HasAnimDictLoaded('mp_suicide') do
-    Wait(150)
-end
-RequestAnimDict('move_m@drunk@verydrunk')
-while not HasAnimDictLoaded('move_m@drunk@verydrunk') do
-    Wait(150)
-end
-RequestAnimDict('drunk_fall_over')
-while not HasAnimDictLoaded('drunk_fall_over') do
-    Wait(150)
-end
+--------------------------------------------
+ped = PlayerPedID()
+Citizen.CreateThread(function()		
+	RequestAnimDict('mp_suicide')
+	while not HasAnimDictLoaded('mp_suicide') do
+    		Wait(150)
+	end
+		
+	RequestAnimDict('move_m@drunk@verydrunk')
+	while not HasAnimDictLoaded('move_m@drunk@verydrunk') do
+    		Wait(150)
+	end
+		
+	RequestAnimSet("move_m@drunk@slightlydrunk")
+	while not HasAnimSetLoaded("move_m@drunk@slightlydrunk") do
+		Wait(150)
+	end
 end)
 
-ped = GetPlayerPed(-1)
-
------------------------------------------------------------------
---															   --
---             NON ESX CODE                                    --
---															   --
------------------------------------------------------------------
 RegisterCommand('cyanide', function(source, args)
-cyanide()    
-end, false)
+	ClearPedTasksImmediately(ped)
+	TaskPlayAnim(ped, "mp_suicide", "pill", 8.0, 1.0, -1, 2, 0, false,false,true)
+	Citizen.Wait(5000)
+	ClearPedTasksImmediately(ped)
+
+      	SetPedMovementClipset(ped, "move_m@drunk@slightlydrunk", true)
+	Citizen.Wait(timeToDie /2 -5000) -- Start effects of the poison
+
+      	SetPedMovementClipset(ped, "move_m@drunk@verydrunk", true)
+      	SetTimecycleModifier("spectator5") -- Drunk visual effects
 
 
-function cyanide()
-ped = GetPlayerPed(-1)
-print("has Taking cyanide")
+	Citizen.Wait(timeToDie) 
+	SetEntityHealth(ped, 0)
 
-ClearPedTasksImmediately(ped)
-
-TaskPlayAnim(ped, "mp_suicide", "pill", 8.0, 1.0, -1, 2, 0, false,false,true)
-Citizen.Wait(5000)
-ClearPedTasksImmediately(ped)
-
-RequestAnimSet("move_m@drunk@slightlydrunk")
-      while not HasAnimSetLoaded("move_m@drunk@slightlydrunk") do
-        Citizen.Wait(0)
-      end
-
-      SetPedMovementClipset(ped, "move_m@drunk@slightlydrunk", true)
-	  print("walk drunk")
-Citizen.Wait(timeToDie /2 -5000) -- Start effects of the poison
-
- RequestAnimSet("move_m@drunk@verydrunk")
-      while not HasAnimSetLoaded("move_m@drunk@verydrunk") do
-        Citizen.Wait(0)
-      end
-
-      SetPedMovementClipset(ped, "move_m@drunk@verydrunk", true)
-	  print("walk drunk")
-      SetTimecycleModifier("spectator5")
-
-
-Citizen.Wait(timeToDie) 
-SetEntityHealth(ped, 0)
-
-if GetEntityHealth(ped, 0) then
-ResetPedMovementClipset(ped)
-        ClearTimecycleModifier()
+	if GetEntityHealth(ped, 0) then
+		ResetPedMovementClipset(ped)
+        	ClearTimecycleModifier()
 		end
-	end
+end, false)
