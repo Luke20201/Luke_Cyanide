2f2b9e10a6d82e0e9016415143820f79dac06f28
Luke <62490330+Luke20201@users.noreply.github.com>
2024-07-17 18:49:17 +0100

    Improve code

--- client.lua
+++ client.lua
@@ -1,37 +1,33 @@
-timeToDie = 60000 --| 60 seconds until the player will die after taking cyanide
+timeToDie = Config.timeToDie --| 60 seconds until the player will die after taking cyanide
 --------------------------------------------
 -- DO NOT TOUCH BELOW--
 --------------------------------------------
 ped = PlayerPedID()
-Citizen.CreateThread(function()		
-	RequestAnimDict('mp_suicide')
-	while not HasAnimDictLoaded('mp_suicide') do
-    		Wait(150)
-	end
-		
-	RequestAnimDict('move_m@drunk@verydrunk')
-	while not HasAnimDictLoaded('move_m@drunk@verydrunk') do
-    		Wait(150)
-	end
-		
-	RequestAnimSet("move_m@drunk@slightlydrunk")
-	while not HasAnimSetLoaded("move_m@drunk@slightlydrunk") do
-		Wait(150)
-	end
-end)
+
+function LoadAnimDict(dict)
+	CreateThread(function()
+		RequestAnimDict(dict)
+		while not HasAnimDictLoaded(dict) do
+    			Wait(150)
+		end
+	end)
+end
+
+LoadAnimDict('mp_suicide')
+LoadAnimDict('move_m@drunk@verydrunk')
+LoadAnimDict("move_m@drunk@slightlydrunk")
 
 RegisterCommand('cyanide', function(source, args)
 	ClearPedTasksImmediately(ped)
-	TaskPlayAnim(ped, "mp_suicide", "pill", 8.0, 1.0, -1, 2, 0, false,false,true)
+	TaskPlayAnim(ped, "mp_suicide", "pill", 8.0, 1.0, -1, 2, 0, false, false, true)
 	Citizen.Wait(5000)
 	ClearPedTasksImmediately(ped)
 
       	SetPedMovementClipset(ped, "move_m@drunk@slightlydrunk", true)
-	Citizen.Wait(timeToDie /2 -5000) -- Start effects of the poison
+	Citizen.Wait(timeToDie / 2 - 5000) -- Start effects of the poison
 
       	SetPedMovementClipset(ped, "move_m@drunk@verydrunk", true)
       	SetTimecycleModifier("spectator5") -- Drunk visual effects
-
 
 	Citizen.Wait(timeToDie) 
 	SetEntityHealth(ped, 0)
@@ -39,5 +35,5 @@ RegisterCommand('cyanide', function(source, args)
 	if GetEntityHealth(ped, 0) then
 		ResetPedMovementClipset(ped)
         	ClearTimecycleModifier()
-		end
+	end
 end, false)
